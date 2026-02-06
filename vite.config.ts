3bee43c4f92dbf2b2cfd9a7725c138f8628190fb
wista-project <wista-project@outlook.jp>
2026-02-06 21:15:22 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/wista-1/" : "/",
+  base: "./", // ← custom domain用に設定
   server: {
     host: "::",
     port: 8080,
